e96f53f6932e97f628997e9aa9427a09602fed8f
DESKTOP-52351T8\Admin <anesk120@naver.com>
2024-10-11 17:31:04 +0900

    1. 플레이어 대기, 걷기, 뛰기 애니메이션 2. 플레이어 반전 구현 3. 플레이어 점프 애니 셋팅

---
+++ Assets/My2D/Scripts/AnimationString.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace My2D
+{
+    //애니메이션 파라미터를 관리하는 클래스
+    public class AnimationString : MonoBehaviour
+    {
+        public static string IsMove = "IsMove";
+        public static string IsRun = "IsRun";
+    }
+}
